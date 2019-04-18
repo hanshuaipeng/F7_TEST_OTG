c81aafae55d70eaf4eda6d7fa91728beeaaa6aa2
hsp <835110287@qq.com>
2019-04-18 14:46:26 +0800

    更改ltdc配置

--- MDK-ARM/TEST/lcd.d
+++ MDK-ARM/TEST/lcd.d
@@ -8,7 +8,7 @@ test\lcd.o: ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_def.h
 test\lcd.o: ../Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f7xx.h
 test\lcd.o: ../Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f767xx.h
 test\lcd.o: ../Drivers/CMSIS/Include/core_cm7.h
-test\lcd.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+test\lcd.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 test\lcd.o: ../Drivers/CMSIS/Include/cmsis_version.h
 test\lcd.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
 test\lcd.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
@@ -16,7 +16,7 @@ test\lcd.o: ../Drivers/CMSIS/Include/mpu_armv7.h
 test\lcd.o: ../Drivers/CMSIS/Device/ST/STM32F7xx/Include/system_stm32f7xx.h
 test\lcd.o: ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal.h
 test\lcd.o: ../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
-test\lcd.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+test\lcd.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
 test\lcd.o: ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc_ex.h
 test\lcd.o: ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_exti.h
 test\lcd.o: ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio.h
@@ -38,7 +38,7 @@ test\lcd.o: ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_tim.h
 test\lcd.o: ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_tim_ex.h
 test\lcd.o: ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart.h
 test\lcd.o: ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart_ex.h
-test\lcd.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+test\lcd.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
 test\lcd.o: ../Inc/sdram.h
 test\lcd.o: ../Inc/main.h
 test\lcd.o: ../Inc/fmc.h
